41fd95544bc55dae1142b680c71a6c2efb91cee6
ByteBuf <104734722+ProxyLoader@users.noreply.github.com>
2025-12-17 20:52:08 +0200

    0034-FIND-FIRST-AND-LAST-POSITION-OF-ELEMENT-IN-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 55.3 MB (54.18%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -5,7 +5,7 @@ var searchRange = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] === target) {
             if (first === -1) first = i;
-            last = i;                  
+            last = i; 
         }
     }
 
