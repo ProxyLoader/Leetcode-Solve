5d3a3640e460becf2d77cd3424732b652e58eff7
ByteBuf <104734722+ProxyLoader@users.noreply.github.com>
2026-01-16 14:00:20 +0200

    0451-SORT-CHARACTERS-BY-FREQUENCY Stats: Time: 11 ms (83.88%), Space: 58.8 MB (80.94%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.js
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.js
@@ -5,19 +5,15 @@
  */
 var frequencySort = function (string) {
     let finalWord = "";
-  const map = new Map();
-  for (let i = 0; i < string.length; i++) {
-    map.set(string[i], (map.get(string[i]) || 0) + 1);
-  }
+    const map = new Map();
+    for(let i = 0; i < string.length; i++){
+        map.set(string[i], (map.get(string[i]) || 0) + 1)
+    }
+    const sortedArray = [...map.entries()].sort((a,b) => b[1] - a[1]);
 
-  const sortedArray = [...map.entries()].sort((a, b) => {
-    return b[1] - a[1];
-  });
+    for(const [char, count] of sortedArray){
+        finalWord += char.repeat(count);
+    }
 
-  for(const [char, count] of sortedArray){
-    finalWord += char.repeat(count);
-  }
-
-  return finalWord;
+    return finalWord;
 };
-
