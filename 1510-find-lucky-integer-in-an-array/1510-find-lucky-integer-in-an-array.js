52f9a44a0922742a83e3539bf80bc87c56c73028
ByteBuf <104734722+ProxyLoader@users.noreply.github.com>
2026-02-26 21:54:50 +0200

    1510-FIND-LUCKY-INTEGER-IN-AN-ARRAY Stats: Time: 3 ms (56.08%), Space: 56 MB (64.71%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.js
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.js
@@ -7,12 +7,11 @@ var findLucky = function (arr) {
   for (let i = 0; i < arr.length; i++) {
     map.set(arr[i], (map.get(arr[i]) || 0) + 1);
   }
-
-  const x = [...map].sort((a, b) => b[1] - a[1]);
-  for(let i = 0; i < x.length; i++){
-    for(j = 0; j < x[i].length; j++){
-        if(x[i][0] === x[i][1]) return x[i][0];
+  let unlucky = -1;
+  for(const [key, value] of map){
+    if(key === value){
+        unlucky = Math.max(unlucky, value);
     }
   }
-  return -1;
+  return unlucky;
 };
