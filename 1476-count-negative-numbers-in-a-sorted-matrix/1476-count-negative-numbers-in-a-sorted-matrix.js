e702ba99ba71ab1d0b0347e4a1eaac52f7c99493
ByteBuf <104734722+ProxyLoader@users.noreply.github.com>
2026-03-06 22:24:28 +0200

    1476-COUNT-NEGATIVE-NUMBERS-IN-A-SORTED-MATRIX Stats: Time: 0 ms (100%), Space: 55 MB (53.22%) - LeetHub

--- 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.js
+++ 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.js
@@ -1,14 +1,20 @@
-/**
- * @param {number[][]} grid
- * @return {number}
- */
-var countNegatives = function (grid) {
-    let neg = 0;
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
-            if (grid[i][j] < 0) neg++;
+var countNegatives = function(grid) {
+    let count = 0;
+    
+    for(let row of grid) {
+        let left = 0, right = row.length - 1;
+        
+        while(left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            if(row[mid] < 0) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
-
+        
+        count += row.length - left;
     }
-    return neg;
+    
+    return count;
 };
