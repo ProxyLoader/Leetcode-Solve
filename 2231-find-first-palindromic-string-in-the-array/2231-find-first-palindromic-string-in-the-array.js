a0aba4878ae03b80c54345898671e495e547334d
ByteBuf <104734722+ProxyLoader@users.noreply.github.com>
2026-03-07 21:10:53 +0200

    2231-FIND-FIRST-PALINDROMIC-STRING-IN-THE-ARRAY Stats: Time: 4 ms (66.67%), Space: 59.1 MB (82.47%) - LeetHub

--- 2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.js
+++ 2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.js
@@ -2,9 +2,24 @@
  * @param {string[]} words
  * @return {string}
  */
-var firstPalindrome = function (words) {
-    for (let i = 0; i < words.length; i++) {
-        if(words[i] === words[i].split("").reverse().join("")) return words[i];
+var firstPalindrome = function(words) {
+
+    const isPalindrome = (word) => {
+        let left = 0;
+        let right = word.length - 1;
+
+        while (left < right) {
+            if (word[left] !== word[right]) return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    };
+
+    for (let word of words) {
+        if (isPalindrome(word)) return word;
     }
+
     return "";
 };
