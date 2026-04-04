dfbb661964e81e059256c0f4ec113f7f73eefa72
ByteBuf <104734722+ProxyLoader@users.noreply.github.com>
2026-04-04 23:50:37 +0300

    0728-SELF-DIVIDING-NUMBERS Stats: Time: 7 ms (27.3%), Space: 56.9 MB (24.34%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.js
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.js
@@ -1,23 +1,12 @@
-/**
- * @param {number} left
- * @param {number} right
- * @return {number[]}
- */
-var selfDividingNumbers = function (left, right) {
-    const list = [];
+var selfDividingNumbers = function(left, right) {
+    const res = [];
+
     for (let i = left; i <= right; i++) {
-        const asStr = i.toString().split("").map((v) => Number(v));
-        let isDivisble = true;
-        if (asStr.includes(0)) continue;
-        for (let j = 0; j < asStr.length; j++) {
-            if (i % asStr[j] !== 0) {
-                isDivisble = false;
-                break;
-            }
-        }
-        if(isDivisble){
-          list.push(i);
+        const s = i.toString();
+        if (!s.includes('0') && [...s].every(d => i % d === 0)) {
+            res.push(i);
         }
     }
-    return list;
+
+    return res;
 };
