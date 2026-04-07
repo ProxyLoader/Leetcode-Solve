c6cd5ab29064739cbbc03b7e3355727a2b846c24
ByteBuf <104734722+ProxyLoader@users.noreply.github.com>
2026-04-07 23:08:14 +0300

    0724-FIND-PIVOT-INDEX Stats: Time: 2 ms (68.06%), Space: 58.4 MB (32.29%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -3,19 +3,14 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-    const totalSum = nums.reduce((acc, num) => acc + num, 0);
-    
+    const totalSum = nums.reduce((x,y) => x +y, 0)
     let leftSum = 0;
-    
-    for (let i = 0; i < nums.length; i++) {
-        const rightSum = totalSum - leftSum - nums[i];
-        
-        if (leftSum === rightSum) {
+    for(let i = 0; i < nums.length; i++){
+        let rightSum = totalSum - leftSum - nums[i];
+        if(leftSum === rightSum) {
             return i;
         }
-        
         leftSum += nums[i];
     }
-    
     return -1;
 };
