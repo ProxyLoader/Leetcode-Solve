c022c48a4d31fbecd8af56e8f07e283539d1d410
ByteBuf <104734722+ProxyLoader@users.noreply.github.com>
2026-04-28 00:56:17 +0300

    Remove blank line in triangle type function
    Removed unnecessary blank line in triangle type function.

--- 3321-type-of-triangle/3321-type-of-triangle.js
+++ 3321-type-of-triangle/3321-type-of-triangle.js
@@ -3,7 +3,6 @@ var triangleType = function (nums) {
     if (a + b <= c || a + c <= b || b + c <= a) {
         return "none";
     }
-    
     if (a === b && b === c) {
         return "equilateral";
     } else if (a === b || a === c || b === c) {
